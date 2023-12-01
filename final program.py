2edd7000c80e63d7b81c9adeac0e1bd5c96db5bc
abbyparr <123125559+abbyparr@users.noreply.github.com>
2023-12-01 12:23:29 -0500

    Update final program.py

--- final program.py
+++ final program.py
@@ -1,30 +1,31 @@
 import seaborn as sns
 from nltk.probability import FreqDist
+import pandas as pd
 #open file, ‘Border_Crossing_Entry_Data’
-
-open('Border_Crossing_Entry_Data', 'r')
+border_data = pd.read_csv('Border_Crossing_Entry_Data.csv')
+print(border_data)
 
 sns.get_dataset_names()
-data = sns.load_dataset('Border_Crossing_Entry_Data')
+data = sns.load_dataset('border_data')
 
 Border_Crossing_Entry_Data.data.shape()
 
 #contrast state/port names
-sns.histplot(x = 'state', hue = 'port names', data = Border_Crossing_Entry_Data)
+sns.histplot(x = 'state', hue = 'port names', data = border_data)
 sns.lineplot(x = 'state', y = 'port names')
 
 #compare border location
-sns.distplot(x = 'border', data = Border_Crossing_Entry_Data)
+sns.distplot(x = 'border', data = border_data)
 #frequency of border location usage
 border1 = FreqDist('US-Mexico Border')
 border2 = FreqDist('US-Canada Border')
 
 #compare date
-sns.histplot(x = 'date', hue = 'month', data = Border_Crossing_Entry_Data)
-sns.lineplot(x = 'date', hue = 'month', data = Border_Crossing_Entry_Data)
+sns.histplot(x = 'date', hue = 'month', data = border_data)
+sns.lineplot(x = 'date', hue = 'month', data = border_data)
 
 #analyze measure(type of transportation crossing)
-sns.distplot(x = 'measure', data = Border_Crossing_Entry_Data)
+sns.distplot(x = 'measure', data = border_data)
 #frequency of measure(type of transportation crossing)
 trucks = FreqDist('trucks')
 trains = FreqDist('trains')
